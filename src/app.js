d9fe2eaab9f1837f6576e7824f3e2cdda29b3bb7
Eduardo Quagliato <eduardo@quagliato.me>
2020-01-16 19:43:01 -0300

    :recycle: Complete migration from git to simple-git.

--- src/app.js
+++ src/app.js
@@ -1,46 +1,55 @@
-const compressor = require('./utils/compressor')
+const fildesExtra = require('fildes-extra')
 const fs = require('fs')
 const githubClient = require('./client/github')
 const gitClient = require('./client/git-simple')
-const fildes = require('fildes-extra')
 const path = require('path')
 
+const {
+  GITHUB_AUTH_TOKEN,
+  GITHUB_AUTH_USER,
+  DEBUG
+} = process.env
+
+const createRemote = (orgHandle, repoName) => `https://${GITHUB_AUTH_USER}:${GITHUB_AUTH_TOKEN}@github.com/${orgHandle}/${repoName}`
 const repoPath = (localPath, repoName) => `${localPath}/${repoName}`
 
 async function processRepo (orgHandle, reposPath, repo) {
   const localPath = `${path.dirname(require.main.filename)}/${reposPath}`
-  // if (fs.existsSync(localPath)) {
-  //   await fildes.rmdir(localPath)
-  // }
+  log(`Processing ${orgHandle}/${repo}...`)
+  return gitClient.openRepo(repoPath(localPath, repo), createRemote(orgHandle, repo))
+    .then(gitRepo => gitClient.fetchAll(gitRepo))
+    .then(gitRepo => gitClient.pullAllBranches(gitRepo))
+}
 
-  // const repoPathA = repoPath(localPath, repo)
-  return gitClient.openRepo(orgHandle, repo, localPath)
-    // .then(() => compressor.compressDir(repoPathA, `${repoPathA}.tar.gz`))
-    // .then(() => {
-    //   log(`Excluindo diretório ${repoPathA}...`)
-    //   return fildes.rmdir(repoPathA)
-    // })
-    // .then(() => {
-    //   log(`Diretório ${repoPathA} excluído.`)
-    //   return repoPathA
-    // })
+function chunkRepos (repos) {
+  const chunkSize = 10
+  const auxArray = []
+  while (repos.length > 0) {
+    auxArray.push(repos.splice(0, chunkSize))
+  }
+
+  return auxArray
 }
 
 async function run () {
   const orgHandle = process.argv[2]
-  const reposPath = `${process.argv[3] || 'localRepos'}`
+  const reposPath = `${process.argv[3] || 'localRepos'}/${orgHandle}`
   
   const exists = fs.existsSync(reposPath)
   if (!exists) {
     log(`Storage directory ${reposPath} does not exist, creating...`)
-    fs.mkdirSync(reposPath)
+    await fildesExtra.mkdir(reposPath)
     log(`Storage directory ${reposPath} created.`)
   }
 
   try {
     const repos = await githubClient.listOrgRepos(orgHandle)
-    for (const repo of repos) {
-      await processRepo(orgHandle, reposPath, repo.name)
+    log(`Chunking repos...`)
+    const chunks = chunkRepos(repos.map(repo => repo.name))
+    log(`${chunks.length} chunks created.`)
+    for (const chunk of chunks) {
+      log(`Processing chunk #${chunks.indexOf(chunk)} of ${chunks.length}.`)
+      await Promise.all(chunk.map(repo => processRepo(orgHandle, reposPath, repo)))
     }
 
     process.exit(0)
--- src/client/git-simple.js
+++ src/client/git-simple.js
@@ -1,21 +1,17 @@
 const git = require('simple-git/promise')
 const {
-  GITHUB_AUTH_TOKEN,
-  GITHUB_AUTH_USER,
   DEBUG
 } = process.env
 
 const isDebuging = () => !!DEBUG;
-const createRemote = (orgHandle, repoName) => `https://${GITHUB_AUTH_USER}:${GITHUB_AUTH_TOKEN}@github.com/${orgHandle}/${repoName}`
-const repoPath = (localPath, repoName) => `${localPath}/${repoName}`
-const gitRepo = (localPath, repoName) => git(repoPath(localPath, repoName)).silent(isDebuging())
 
-const cloneRepo = (orgHandle, repoName, localPath) => {
+const cloneRepo = (repoPath, remote) => {
+  log(`Cloning repo...`)
   return git()
-    .clone(createRemote(orgHandle, repoName), repoPath(localPath, repoName))
+    .clone(remote, repoPath)
     .then(() => {
-      log(`Repo ${orgHandle}/${repoName} cloned.`)
-      return openRepo(orgHandle, repoName, localPath)
+      log(`Repo cloned.`)
+      return openRepo(repoPath)
     })
     .catch(err => {
       log(err, 'CRITICAL')
@@ -23,71 +19,64 @@ const cloneRepo = (orgHandle, repoName, localPath) => {
     })
 }
 
-const openRepo = (orgHandle, repoName, localPath) => {
-  return git(repoPath(localPath, repoName))
-    .silent(isDebuging)
+const openRepo = (repoPath, remote) => {
+  const gitRepo = git(repoPath).silent(isDebuging())
+  return gitRepo
     .status()
+    .then(() => gitRepo)
     .catch(err => {
-      log(`Repo ${orgHandle}/${repoName} is not cloned, it will be cloned.`)
-      return cloneRepo(orgHandle, repoName, localPath)
+      log(`Repo is not cloned, it will be cloned.`)
+      return cloneRepo(repoPath, remote)
     })
-    .then(() => fetchAll(orgHandle, repoName, localPath))
 }
 
-const fetchAll = (orgHandle, repoName, localPath) => {
-  log(`Fetching all of repo ${orgHandle}/${repoName}`);
-  return branches(orgHandle, repoName, localPath)
-    .then(branchList => {
-      console.log(branchList)
-      return branchList.map(branchName => fetch(orgHandle, repoName, localPath, branchName))
-    })
+const fetchAll = (repo) => {
+  return repo
+    .fetch()
+    .then(() => repo)
     .catch(err => {
       log(err, 'CRITICAL')
       throw new Error(`It was not possible to fetch the branches for this repo.`)
     })
 }
 
-const checkout = (orgHandle, repoName, localPath, branch) => {
-  return gitRepo(localPath, repoName)
-    .checkoutLocalBranch(branch)
-    .pull()
-    .checkoutLocalBranch('master')
-    .then(() => {
-      return repoPath(localPath, repoName)
+const pullAllBranches = async (repo) => {
+  const branchesList = await branches(repo);
+  for (let branchName of branchesList) {
+    await pull(repo, branchName)
+  }
+
+  return repo
+}
+
+const branches = (repo) => {
+  return repo
+    .branch()
+    .then(branchesResponse => branchesResponse.all.map(branchName => branchName.replace('remotes/origin/', '')))
+    .then(branchesList => {
+      log(`${branchesList.length} branches found.`)
+      return branchesList
     })
+    .catch(err => {
+      log(err, 'CRITICAL')
+      throw new Error('It was not possible to fetch branchs for this rbranchesReponse.all)epo.')
+    })
+}
+
+const pull = (repo, branch) => {
+  log(`Checkouting branch ${branch}`);
+  return repo
+    .checkout(branch)
+    .then(() => repo.pull('origin', branch))
+    .then(() => repo)
     .catch(err => {
       log(err, 'CRITICAL')
       throw new Error(`Could not pull branch ${branch} of this repo.`)
     })
 }
 
-const fetch = (orgHandle, repoName, localPath, branch) => {
-  log(`Fetching branch ${branch} of repo ${orgHandle}/${repoName}`);
-  return gitRepo(localPath, repoName)
-    .fetch(createRemote(orgHandle, repoName), branch)
-    .then(() => checkout(orgHandle, repoName, localPath, branch))
-    .catch(err => {
-      log(err, 'CRITICAL')
-      throw new Error(`It was not possible to fetch the branch ${branch} for this repo.`)
-    })
-}
-
-const branches = (orgHandle, repoName, localPath) => {
-  log(`Listing branches of repo ${orgHandle}/${repoName}`);
-  return gitRepo(localPath, repoName)
-    .branch()
-    .then(branchesReponse => {
-      log(`${branchesReponse.all.length} branches found.`)
-      return branchesReponse.all
-    })
-    .catch(err => {
-      log(err, 'CRITICAL')
-      throw new Error('It was not possible to fetch branchs for this repo.')
-    })
-}
-
 module.exports = {
   openRepo,
-  cloneRepo,
-  fetchAll
+  fetchAll,
+  pullAllBranches
 }
